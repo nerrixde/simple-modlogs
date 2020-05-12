e69d51bbd8a1fa58d21f6f854801726a9dec5f22
Nerrix <30556676+nerrixDE@users.noreply.github.com>
2020-05-12 17:04:35 +0200

    Create configs.py

---
+++ configs.py
@@ -0,0 +1,10 @@
+DISCORD_BOT_TOKEN = "Your-Bot-Token-Here"
+
+
+WATCHING_STATUS = "for infringements"
+
+MUTED_ROLE_NAME = "Muted"
+MOD_LOG_CHANNEL_NAME = "mod-log"
+
+
+MODLOG_COLORS = {"BAN": 0xeb4034, "MUTE": 0xeda239, "UNMUTE": 0x56c470, "UNBAN": 0x4fb09e, "KICK": 0x559ced}
